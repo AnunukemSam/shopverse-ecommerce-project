42b1c7487b6f9487bd4010a0355b024a5fdeddf7
Sam Chukwuebuka Anunukem <samuelanunukem@gmail.com>
2025-04-07 14:10:31 +0100

    Create productRoutes.js

---
+++ services/product/routes/productRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const Product = require("../models/Product");
+
+// Get all products
+router.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+// Get product by ID
+router.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) return res.status(404).send("Product not found");
+  res.json(product);
+});
+
+// Create product
+router.post("/", async (req, res) => {
+  const product = new Product(req.body);
+  await product.save();
+  res.status(201).json(product);
+});
+
+// Update product
+router.put("/:id", async (req, res) => {
+  const updated = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updated);
+});
+
+// Delete product
+router.delete("/:id", async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.status(204).end();
+});
+
+module.exports = router;
